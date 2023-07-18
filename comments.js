27fcbdf257da6702d08715e55893898e77b0340c
Oludot1 <46471858+Oludot1@users.noreply.github.com>
2023-07-18 17:06:25 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,56 @@
+// Create web server
+// 1. npm install express
+// 2. npm install body-parser
+// 3. npm install cors
+// 4. npm install --save-dev nodemon
+
+// 5. npm install mongoose
+// 6. npm install dotenv
+// 7. npm install bcryptjs
+// 8. npm install jsonwebtoken
+// 9. npm install passport
+// 10. npm install passport-jwt
+// 11. npm install passport-local
+// 12. npm install validator
+
+// 13. npm install axios
+// 14. npm install react-router-dom
+// 15. npm install react-redux
+// 16. npm install redux
+// 17. npm install redux-thunk
+
+// 18. npm install classnames
+// 19. npm install jwt-decode
+// 20. npm install moment
+// 21. npm install react-moment
+// 22. npm install react-textarea-autosize
+// 23. npm install react-transition-group
+// 24. npm install react-twitter-widgets
+// 25. npm install reactjs-popup
+// 26. npm install react-scroll-up-button
+// 27. npm install react-share
+// 28. npm install react-simple-chatbot
+// 29. npm install react-toastify
+// 30. npm install reactjs-popup
+// 31. npm install react-scroll-up-button
+// 32. npm install react-share
+// 33. npm install react-simple-chatbot
+// 34. npm install react-toastify
+// 35. npm install reactjs-popup
+// 36. npm install react-scroll-up-button
+// 37. npm install react-share
+// 38. npm install react-simple-chatbot
+// 39. npm install react-toastify
+// 40. npm install reactjs-popup
+// 41. npm install react-scroll-up-button
+// 42. npm install react-share
+// 43. npm install react-simple-chatbot
+// 44. npm install react-toastify
+// 45. npm install reactjs-popup
+// 46. npm install react-scroll-up-button
+// 47. npm install react-share
+// 48. npm install react-simple-chatbot
+// 49. npm install react-toastify
+// 50. npm install reactjs-popup
+// 51. npm install react-scroll-up-button
+//
